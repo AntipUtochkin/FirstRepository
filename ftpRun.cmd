157d3716a8539a0eb9e746c20aaab20e66285bb9
AntipUtochkin <57570760+AntipUtochkin@users.noreply.github.com>
2020-01-07 01:20:13 +0500

    Update ftpRun.cmd

--- ftpRun.cmd
+++ ftpRun.cmd
@@ -1,1 +1,12 @@
 echo Send file
+@ftp -i -s:"%~f0"&GOTO:EOF
+open files.000webhost.com
+unrated-circuitry
+cWou@UfKhb)RftnDwjVS
+cd public_html
+pwd
+put index.html
+disconnect
+rem bye
+rem cmd
+quit
